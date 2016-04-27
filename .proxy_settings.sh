8c5c6ffec464f9e850757efce4871fd11738701f
Rahul Nair <rahulunair@gmail.com>
2016-04-27 13:38:24 -0500

    my dot files

---
+++ .proxy_settings.sh
@@ -0,0 +1,23 @@
+function proxy_on() {
+    export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
+    export http_proxy="http://proxy-chain.intel.com:911/"
+    export https_proxy=$http_proxy
+    export ftp_proxy=$http_proxy
+    export rsync_proxy=$http_proxy
+    export HTTP_PROXY=$http_proxy
+    export HTTPS_PROXY=$http_proxy
+    export FTP_PROXY=$http_proxy
+    export RSYNC_PROXY=$http_proxy
+}
+
+function proxy_off(){
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+    unset rsync_proxy
+    echo -e "Proxy environment variable removed."
+}
+
+proxy_on
+
+
